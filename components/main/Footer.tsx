3960f080b0be942cf9e711dda4fafa40b66ddad2
konstantin <konstantinivanovic12345@gmail.com>
2024-04-09 20:30:50 -0700

    public pics added

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -6,7 +6,6 @@ import {
   RxTwitterLogo,
   RxLinkedinLogo,
 } from "react-icons/rx";
-import Link from 'next/link';
 
 import { FaYoutube } from "react-icons/fa";
 
@@ -18,15 +17,21 @@ const Footer = () => {
                 
 
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
+                    <div className="font-bold text-[16px] cursor-pointer">Community</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
+                        <a 
+                        className="text-[15px] ml-[6px] cursor-pointer">
+                            Youtube
+                        </a>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
                         
-                            <a className="text-[15px] ml-[6px] cursor-pointer">Github</a>
+                        <a href="https://github.com/Konstantin907" className="cursor-pointer text-[15px] ml-[6px]" target="_blank" rel="noopener noreferrer">
+                            Github
+                        </a>
+
                         
                             
                     </p>
@@ -47,28 +52,26 @@ const Footer = () => {
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxDiscordLogo />
+                        {/*  */}
                         <span className="text-[15px] ml-[6px]">Linkedin</span>    
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                     
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
+                    <div className="font-bold text-[16px] mt-[-60px]">About</div>
+                   
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
                         <span className="text-[15px] ml-[6px]">Learning about me</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <span className="text-[15px] ml-[6px]">konstantinivanovic12345@gmail.com</span>    
                     </p>
                 </div>
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; Konstantin Ivanovic . All rights reserved
             </div>
         </div>
     </div>
